9de08d3dc0a37870a3b7cdd584312f66adb8e9a4
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-25 03:12:04 +0000

    commit four

--- lib/cli_job_search/api.rb
+++ lib/cli_job_search/api.rb
@@ -2,9 +2,13 @@ require_relative '../../config/environment'
 
 
 class API
+
   def self.get_data
-   response = HTTParty.get ("https://jobs.github.com/positions.json?description=python&location=washington+dc")
-   binding.pry
+   response = HTTParty.get ("https://jobs.github.com/positions.json?description=ruby&search=code&page=0")
+     response.each do |hash|
+       hash.select['location']
+        binding.pry
+    end
   end
 end
 
