e9fabc3effbbdf3325313278ab08806b828f72f7
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-03 03:44:40 +0000

    update list revamp

--- lib/cli_job_search/cli.rb
+++ lib/cli_job_search/cli.rb
@@ -2,17 +2,18 @@ class CLI
 
  def open
    #API.get_data
+  self.welcome
   self.menu
  end
 
  def welcome
   puts "Hello, happy to help you search for Ruby on Rails developer positions."
-    sleep(2)
+   sleep(2)
     API.get_data
   end
 
  def menu
-  welcome
+   puts "Would you like to continue or exit?"
    puts "1. Continue"
    puts "2. Exit"
    puts "Please choose an option using a number:"
@@ -29,7 +30,6 @@ class CLI
  def display_jobs
    JOB.all.each_with_index do |job, index|
     puts "#{index + 1}. #{job.title} - #{job.location}"
-    puts""
     end
  end
 
@@ -39,7 +39,8 @@ class CLI
     puts "No jobs found!"
     puts ""
     puts "Here's a list of jobs and locations."
-    display_jobs
+    job_choices
+    system ('clear')
     menu
   else
   j.each do |j|
@@ -49,14 +50,19 @@ class CLI
     puts "Company: #{j.company}"
     puts "Location: #{j.location}"
     puts "URL: #{j.url}"
-    puts "------------------Description------------------------------"
+    # puts "------------------Description------------------------------"
+    # puts ""
+    # re =  /<("[^"]*"|'[^']*'|[^'">])*>/
+    # puts j.description.gsub!(re, '')
+    puts "------------------------------------------------------------"
+    display_jobs
     puts ""
-    re =  /<("[^"]*"|'[^']*'|[^'">])*>/
-    puts j.description.gsub!(re, '')
-
-   end
+    job_choices
+    menu
+    end
   end
  end
+
 
  def users_selection(input)
    case input
@@ -69,6 +75,40 @@ class CLI
       job_selection(locations)
     end
   end
+
+  def jobs_details
+    puts "--------------Job Details--------------------"
+    puts "Title: #{@job.title}"
+    puts "Company: #{@job.company}"
+    puts "Location: #{@job.location}"
+    puts "URL: #{@job.url}"
+    puts "----------------------------------------------"
+  end
+
+
+    # re =  /<("[^"]*"|'[^']*'|[^'">])*>/
+    # puts "Description:" + @job.description.gsub!(re, '')
+# user_input = gets.strip.to_i
+# if user_input > 0 && user_input < 21
+# @job = JOB.all[user_input - 1]
+
+
+
+ def job_choices
+  puts "Select a job of choice to see more details:"
+  user_input = gets.chomp.to_i
+  if user_input > 0 && user_input < 31
+    @job = JOB.all[user_input - 1]
+     self.jobs_details
+   else
+     puts "Invalid input, please input a number from the list"
+       self.job_choices
+   end
+ end
+
+def job_description
+ #no code yet
+end
 
 def close
    puts "Have a great day, goodbye"
